527e075d28521ddc4c296da18dd8f1f0f96ee6bc
unknown <nebtiem@gmail.com>
2026-05-07 15:46:46 +0200

    Add art piece details page and link gallery items

--- components/ArtList/Index.js
+++ components/ArtList/Index.js
@@ -1,14 +1,17 @@
 import ArtPreview from "../ArtPreview/Index";
+import Link from "next/link";
 
 export default function ArtList({ artpieces }) {
   return (
     <ul>
       {artpieces.map((artpiece) => (
         <li key={artpiece.slug}>
-          <ArtPreview artpiece={artpiece}></ArtPreview>
-          <p>
-            {artpiece.name} by {artpiece.artist}
-          </p>{" "}
+          <Link href={`/artpieces/${artpiece.slug}`}>
+            <ArtPreview artpiece={artpiece} />
+            <p>
+              {artpiece.name} by {artpiece.artist}
+            </p>
+          </Link>
         </li>
       ))}
     </ul>
---
+++ pages/artpieces/[slug].js
@@ -0,0 +1,38 @@
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
+
+// Notice we added { allImages } as a prop here!
+export default function ArtPieceDetailsPage({ allImages }) {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const piece = allImages?.find((p) => p.slug === slug);
+
+  if (!router.isReady) return <div>Loading...</div>;
+
+  if (!piece) return <div>Art piece not found.</div>;
+
+  return (
+    <>
+      <Link href="/artpieces">← Back to Gallery</Link>
+      <h1>{piece.name}</h1>
+      <p>
+        <strong>Artist:</strong> {piece.artist}
+      </p>
+      <p>
+        <strong>Year:</strong> {piece.year}
+      </p>
+      <p>
+        <strong>Genre:</strong> {piece.genre}
+      </p>
+
+      <Image
+        src={piece.imageSource}
+        alt={piece.name}
+        width={500}
+        height={500}
+      />
+    </>
+  );
+}
