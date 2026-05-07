7ccb2c5bfeab49b719e86e450cf3e32994912932
Artemiy <artemiy.shokin@gmail.com>
2026-05-07 12:18:28 +0200

    cleaned up code and prepared two arrays for future favorites

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,24 @@
 import ArtList from "@/components/ArtList/Index";
+import useSWR from "swr";
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 export default function HomePage() {
+  const { data, error, isLoading } = useSWR(
+    `https://example-apis.vercel.app/api/art`,
+    fetcher
+  );
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+  // render data
+
+  const allImages = data.map((artpiece) => {
+    return { isFavorite: false, ...artpiece };
+  });
+
+  // const favoriteImages = data.filter((artpiece) => artpiece.isfavorite);
   return (
     <div>
-      <ArtList />
+      <ArtList artpieces={allImages} />
     </div>
   );
 }
