87455a77b3e9f310fc94941cf6bf16bee9defa54
Olivia <oliviabelu@web.de>
2026-05-07 16:52:21 +0200

    further tasks

--- pages/favorites/index.js
+++ pages/favorites/index.js
@@ -1,1 +1,7 @@
-export default function Favorites() {}
+import ArtList from "@/components/ArtList/Index";
+
+export default function Favorites({ allImages }) {
+  const favoriteArtpieces = allImages.filter((artpiece) => artpiece.isFavorite);
+
+  return <ArtList artpieces={favoriteArtpieces} />;
+}
