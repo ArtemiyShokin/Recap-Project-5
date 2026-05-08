12a9449d4a3d572ab850f7ce8dd5c3fdfdf64a10
Artemiy <artemiy.shokin@gmail.com>
2026-05-08 18:58:02 +0200

    started figuring out the preview container styling

--- components/ArtList/Index.js
+++ components/ArtList/Index.js
@@ -7,30 +7,44 @@ export default function ArtList({
   artpiecesInfo,
 }) {
   return (
-    <ul>
-      {artpieces.map((artpiece) => {
-        const { isFavorite } = artpiecesInfo?.find(
-          (info) => info.slug === artpiece.slug
-        ) ?? { isFavorite: false };
-        return (
-          <StyledListElement key={artpiece.slug} isFavorite={isFavorite}>
-            <ArtPreview
-              artpiece={artpiece}
-              onToggleFavorite={onToggleFavorite}
-              isFavorite={isFavorite}
-            />
-            <p>
-              {artpiece.name} by {artpiece.artist}
-            </p>
-          </StyledListElement>
-        );
-      })}
-    </ul>
+    <ListContainer>
+      <StyledUnorderedList>
+        {artpieces.map((artpiece) => {
+          const { isFavorite } = artpiecesInfo?.find(
+            (info) => info.slug === artpiece.slug
+          ) ?? { isFavorite: false };
+          return (
+            <StyledListElement key={artpiece.slug} isFavorite={isFavorite}>
+              <ArtPreview
+                artpiece={artpiece}
+                onToggleFavorite={onToggleFavorite}
+                isFavorite={isFavorite}
+              />
+              <p>
+                {artpiece.name} by {artpiece.artist}
+              </p>
+            </StyledListElement>
+          );
+        })}
+      </StyledUnorderedList>
+    </ListContainer>
   );
 }
 
 // hier brauchen wir  styled component ListItem mit : if isFavorite background rot else nix
+const ListContainer = styled.div``;
+
 const StyledListElement = styled.li`
   background-color: ${(props) => (props.isFavorite ? "green" : "transparent")};
-  max-width: 450px;
+`;
+const StyledUnorderedList = styled.ul`
+  display: grid;
+  grid-template:
+    1,
+    20vh / repeat (auto, 1fr);
+  margin-left: 0;
+  list-style: none;
+  padding: 0;
+  justify-items: center;
+  gap: 80px;
 `;
--- components/ArtPreview/Index.js
+++ components/ArtPreview/Index.js
@@ -1,22 +1,51 @@
 import Image from "next/image";
 import Link from "next/link";
 import FavoriteButton from "../FavoriteButton/Index";
+import styled from "styled-components";
 export default function ArtPreview({ artpiece, onToggleFavorite, isFavorite }) {
   return (
     <>
-      <Link href={`/artpieces/${artpiece.slug}`}>
-        <Image
-          src={artpiece.imageSource}
-          alt={artpiece.slug}
-          width="400"
-          height="200"
-        />{" "}
-      </Link>
-      <FavoriteButton
-        onToggleFavorite={onToggleFavorite}
-        artpiece={artpiece}
-        isFavorite={isFavorite}
-      />
+      <PreviewContainer>
+        <StyledFavoriteButton
+          onToggleFavorite={onToggleFavorite}
+          artpiece={artpiece}
+          isFavorite={isFavorite}
+        />
+        <Link href={`/artpieces/${artpiece.slug}`}>
+          <Image
+            src={artpiece.imageSource}
+            alt={artpiece.slug}
+            width={artpiece.dimensions.width}
+            height={artpiece.dimensions.height}
+          />
+        </Link>
+      </PreviewContainer>
     </>
   );
 }
+
+const StyledFavoriteButton = styled(FavoriteButton)`
+  background-color: red;
+`;
+
+const PreviewContainer = styled.div`
+  width: 400px;
+  height: 20vh;
+  overflow: hidden;
+  background-size: cover;
+  background-position: center;
+  border-radius: 20px;
+  border-color: red;
+  border-style: solid;
+  display: flex;
+`;
+
+//  width: "400px",
+//             height: "200px",
+//             overflow: "hidden",
+//             position: "relative",
+
+//  style={{
+//           backgroundSize: "cover",
+//           backgroundPosition: "center",
+//         }}
