a9af7bcf0f8b0573492468ab85af9e40492e720a
Olivia <oliviabelu@web.de>
2026-05-08 14:37:47 +0200

    finished color palette display

--- components/ColorPalette/Index.js
+++ components/ColorPalette/Index.js
@@ -3,15 +3,25 @@ import styled from "styled-components";
 export default function ColorPalette({ colors }) {
   return (
     <>
-      {colors.map((color, index) => (
-        <StyledDiv key={index} $color={color}>
-          {color}
-        </StyledDiv>
-      ))}
+      <StyledColorCotainer>
+        {colors.map((color, index) => (
+          <StyledDiv key={index} $color={color} />
+        ))}
+      </StyledColorCotainer>
     </>
   );
 }
 
 const StyledDiv = styled.div`
+  width: 3rem;
+  height: 3rem;
   background-color: ${(props) => props.$color};
+  border: 1px solid black;
+  border-radius: 100%;
+`;
+
+const StyledColorCotainer = styled.div`
+  display: flex;
+  gap: 0.2rem;
+  justify-content: center;
 `;
