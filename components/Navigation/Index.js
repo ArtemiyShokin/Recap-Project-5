5064bd541326ea07d75909a9bd4fb2e75950c02b
Olivia <oliviabelu@web.de>
2026-05-10 22:06:02 +0200

    one version of navbar purple

--- components/Navigation/Index.js
+++ components/Navigation/Index.js
@@ -1,18 +1,32 @@
 import Link from "next/link";
-import styled from "styled-components";
+import { useRouter } from "next/router";
+import styled, { css } from "styled-components";
 
 export default function Navigation() {
+  const { pathname } = useRouter();
+  const splittedPathname = pathname.split("/");
+  const slash = "/";
+  const currentPath = slash.concat(splittedPathname[1]);
+
+  console.log(currentPath);
+
   return (
     <NavigationWrapper>
       <StyledList>
         <li>
-          <StyledLink href="/">Spotlight</StyledLink>
+          <StyledLink href="/" $currentPath={currentPath}>
+            Spotlight
+          </StyledLink>
         </li>
         <li>
-          <StyledLink href="/artpieces">Gallery</StyledLink>
+          <StyledLink href="/artpieces" $currentPath={currentPath}>
+            Gallery
+          </StyledLink>
         </li>
         <li>
-          <StyledLink href="/favorites">Favorites</StyledLink>
+          <StyledLink href="/favorites" $currentPath={currentPath}>
+            Favorites
+          </StyledLink>
         </li>
       </StyledList>
     </NavigationWrapper>
@@ -23,16 +37,17 @@ const NavigationWrapper = styled.nav`
   width: 100%;
   position: fixed;
   bottom: 0;
-  background-color: hsl(137, 36%, 85%);
+  background-color: hsl(233, 46%, 65%); // --secondary-400
   border-top-left-radius: 20px;
   border-top-right-radius: 20px;
-  border: 0.2rem outset #ee5324;
+  border: 0.4rem outset hsl(233, 46%, 75%); //--secondary-300
 `;
 
 const StyledList = styled.ul`
   list-style: none;
   display: flex;
-  gap: 4rem;
+  gap: 5rem;
+
   justify-content: center;
   margin: 0;
   padding: 1rem;
@@ -40,12 +55,22 @@ const StyledList = styled.ul`
 
 const StyledLink = styled(Link)`
   text-decoration: none;
-  color: #3e4aa8;
+  color: hsl(233, 46%, 95%); // --secondary-100
   font-weight: bold;
 
   &:hover {
-    color: #ee5324;
+    color: hsl(233, 46%, 85%); // --secondary-200
   }
+  ${(props) =>
+    props.href === props.$currentPath &&
+    css`
+      color: hsl(233, 46%, 25%); //--secondary-800
+      padding: 1rem 0;
+
+      &:hover {
+        color: hsl(233, 46%, 45%); //--secondary-600
+      }
+    `};
 `;
 
 // const NavigationButton = styled.button`
