225c7fe57ec368364ef329b01ff83283c6c3caa9
unknown <nebtiem@gmail.com>
2026-05-11 09:55:06 +0200

    changed details style

--- components/ArtPreview/Index.js
+++ components/ArtPreview/Index.js
@@ -1,16 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
 import FavoriteButton from "../FavoriteButton/Index";
+import { motion } from "framer-motion";
+
+const MotionImage = motion(Image);
 export default function ArtPreview({ artpiece, onToggleFavorite, isFavorite }) {
   return (
     <>
       <Link href={`/artpieces/${artpiece.slug}`}>
-        <Image
+       <MotionImage
           src={artpiece.imageSource}
           alt={artpiece.slug}
           width="400"
           height="200"
-        />{" "}
+          layoutId={artpiece.slug} // 3. The "magic" property
+          transition={{ duration: 0.5 }}
+        />
       </Link>
       <FavoriteButton
         onToggleFavorite={onToggleFavorite}
